f788daa11c07be7658109b3ac6cba2445a4b94c2
ShadowDrake21 <dimka670020040@gmail.com>
2026-06-06 10:33:26 +0200

    development: ADC + DMA Continuous Sampling.
    added DMA and ADC structs. in process of development using the Claude Pro as my strict tutor who navigates and challenges me

--- stm32f411.h
+++ stm32f411.h
@@ -82,6 +82,99 @@ typedef struct
     volatile uint32_t GTPR;  // 0x18
 } USART_RegDef_t;
 
+typedef struct
+{
+    volatile uint32_t LISR;   // 0x0000
+    volatile uint32_t HISR;   // 0x0004
+    volatile uint32_t LIFCR;  // 0x0008
+    volatile uint32_t HIFCR;  // 0x000C
+    volatile uint32_t S0CR;   // 0x0010
+    volatile uint32_t S0NDTR; // 0x0014
+    volatile uint32_t S0PAR;  // 0x0018
+    volatile uint32_t S0M0AR; // 0x001C
+    volatile uint32_t S0M1AR; // 0x0020
+    volatile uint32_t S0FCR;  // 0x0024
+    volatile uint32_t S1CR;   // 0x0028
+    volatile uint32_t S1NDTR; // 0x002C
+    volatile uint32_t S1PAR;  // 0x0030
+    volatile uint32_t S1M0AR; // 0x0034
+    volatile uint32_t S1M1AR; // 0x0038
+    volatile uint32_t S1FCR;  // 0x003C
+    volatile uint32_t S2CR;   // 0x0040
+    volatile uint32_t S2NDTR; // 0x0044
+    volatile uint32_t S2PAR;  // 0x0048
+    volatile uint32_t S2M0AR; // 0x004C
+    volatile uint32_t S2M1AR; // 0x0050
+    volatile uint32_t S2FCR;  // 0x0054
+    volatile uint32_t S3CR;   // 0x0058
+    volatile uint32_t S3NDTR; // 0x005C
+    volatile uint32_t S3PAR;  // 0x0060
+    volatile uint32_t S3M0AR; // 0x0064
+    volatile uint32_t S3M1AR; // 0x0068
+    volatile uint32_t S3FCR;  // 0x006C
+    volatile uint32_t S4CR;   // 0x0070
+    volatile uint32_t S4NDTR; // 0x0074
+    volatile uint32_t S4PAR;  // 0x0078
+    volatile uint32_t S4M0AR; // 0x007C
+    volatile uint32_t S4M1AR; // 0x0080
+    volatile uint32_t S4FCR;  // 0x0084
+    volatile uint32_t S5CR;   // 0x0088
+    volatile uint32_t S5NDTR; // 0x008C
+    volatile uint32_t S5PAR;  // 0x0090
+    volatile uint32_t S5M0AR; // 0x0094
+    volatile uint32_t S5M1AR; // 0x0098
+    volatile uint32_t S5FCR;  // 0x009C
+    volatile uint32_t S6CR;   // 0x00A0
+    volatile uint32_t S6NDTR; // 0x00A4
+    volatile uint32_t S6PAR;  // 0x00A8
+    volatile uint32_t S6M0AR; // 0x00AC
+    volatile uint32_t S6M1AR; // 0x00B0
+    volatile uint32_t S6FCR;  // 0x00B4
+    volatile uint32_t S7CR;   // 0x00B8
+    volatile uint32_t S7NDTR; // 0x00BC
+    volatile uint32_t S7PAR;  // 0x00C0
+    volatile uint32_t S7M0AR; // 0x00C4
+    volatile uint32_t S7M1AR; // 0x00C8
+    volatile uint32_t S7FCR;  // 0x00CC
+} DMA_RegDef_t;
+
+typedef struct
+{
+    volatile uint32_t SR;    // 0x00
+    volatile uint32_t CR1;   // 0x04
+    volatile uint32_t CR2;   // 0x08
+    volatile uint32_t SMPR1; // 0x0C
+    volatile uint32_t SMPR2; // 0x10
+    volatile uint32_t JOFR1; // 0x14
+    volatile uint32_t JOFR2; // 0x18
+    volatile uint32_t JOFR3; // 0x1C
+    volatile uint32_t JOFR4; // 0x20
+    volatile uint32_t HTR;   // 0x24
+    volatile uint32_t LTR;   // 0x28
+    volatile uint32_t SQR1;  // 0x2C
+    volatile uint32_t SQR2;  // 0x30
+    volatile uint32_t SQR3;  // 0x34
+    volatile uint32_t JSQR;  // 0x38
+    volatile uint32_t JDR1;  // 0x3C
+    volatile uint32_t JDR2;  // 0x40
+    volatile uint32_t JDR3;  // 0x44
+    volatile uint32_t JDR4;  // 0x48
+    volatile uint32_t DR;    // 0x4C
+} ADC_RegDef_t;
+
+typedef struct
+{
+    uint32_t RESERVED;     // 0x00
+    volatile uint32_t CCR; // 0x04
+} ADC_CommonRegisters_RegDef_t;
+
+typedef struct
+{
+    ADC_RegDef_t ADC1;
+    uint32_t RESERVED[172]; // 0x50 - 0x2FC -> 0x2B0 -> 688 bytes in total
+    ADC_CommonRegisters_RegDef_t CommonRegisters;
+} ADC_Global_RegDef_t;
+
 // Alternate Function Modes
 typedef enum
 {
@@ -104,23 +197,52 @@ typedef enum
 
 } GPIO_AF_t;
 
-// Base addresses
+// ================== Base addresses ==================
+
 #define RCC_BASE 0x40023800UL
+
 #define TIM2_BASE 0x40000000UL
 #define TIM3_BASE 0x40000400UL
 #define TIM4_BASE 0x40000800UL
 #define TIM5_BASE 0x40000C00UL
+
 #define GPIOA_BASE 0x40020000UL
 #define GPIOB_BASE 0x40020400UL
+#define GPIOC_BASE 0x40020800UL
+#define GPIOD_BASE 0x40020C00UL
+#define GPIOE_BASE 0x40021000UL
+#define GPIOH_BASE 0x40021C00UL
+
 #define USART2_BASE 0x40004400UL
 
+#define DMA1_BASE 0x40026000UL
+#define DMA2_BASE 0x40026400UL
+
+#define ADC1_BASE 0x40012000UL
+
+#
+
+// ================== Registers ==================
+
 #define RCC ((RCC_RegDef_t *)RCC_BASE)
+
 #define TIM2 ((TIM_RegDef_t *)TIM2_BASE)
 #define TIM3 ((TIM_RegDef_t *)TIM3_BASE)
 #define TIM4 ((TIM_RegDef_t *)TIM4_BASE)
 #define TIM5 ((TIM_RegDef_t *)TIM5_BASE)
+
 #define GPIOA ((GPIO_RegDef_t *)GPIOA_BASE)
 #define GPIOB ((GPIO_RegDef_t *)GPIOB_BASE)
+#define GPIOC ((GPIO_RegDef_t *)GPIOC_BASE)
+#define GPIOD ((GPIO_RegDef_t *)GPIOD_BASE)
+#define GPIOE ((GPIO_RegDef_t *)GPIOE_BASE)
+#define GPIOH ((GPIO_RegDef_t *)GPIOH_BASE)
+
 #define USART2 ((USART_RegDef_t *)USART2_BASE)
 
+#define DMA1 ((DMA_RegDef_t *)DMA1_BASE)
+#define DMA2 ((DMA_RegDef_t *)DMA2_BASE)
+
+#define ADC ((ADC_Global_RegDef_t *)ADC1_BASE)
+#define ADC1 ((ADC_RegDef_t *)ADC1_BASE)
 #endif
